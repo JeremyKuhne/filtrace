fe42fc68213db7739fd96590efb86084dde114ed
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2026-09-05 10:23:02 -0700

    Standardize source file headers

--- src/Filtrace.Core/Tracing/CallerRow.cs
+++ src/Filtrace.Core/Tracing/CallerRow.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 Jeremy W Kuhne
+// Copyright (c) Jeremy W Kuhne and contributors
 // SPDX-License-Identifier: MIT
 // See LICENSE file in the project root for full license information
 
